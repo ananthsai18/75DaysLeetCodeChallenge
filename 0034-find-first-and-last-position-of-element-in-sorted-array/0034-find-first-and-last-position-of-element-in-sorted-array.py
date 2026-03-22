c51d4f79f72b491e43f6f2466d4f4629f1b585f8
Ananthsai <ananthsaigudla1718@gmail.com>
2026-03-22 15:38:59 +0530

    Time: 0 ms (100%), Space: 20.5 MB (79.01%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,31 +1,25 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        sm=-1
-        n=len(nums)
-        r= n-1
-        l=0
-        while(l<=r):
-            m=(l+r)//2
-            if nums[m]==target:
-                sm= m
-                r=m-1
-            elif nums[m]>target:
-                r=m-1
-            else:
-                l=m+1
-                
-        lm=-1
-        n=len(nums)
-        r= n-1
-        l=0
-        while(l<=r):
-            m=(l+r)//2
-            if nums[m]==target:
-                lm=m
-                l=m+1
-            elif nums[m]>target:
-                r=m-1
-            else:
-                l=m+1
-        return [sm,lm]
+        def calc(nums,target,isStart):
+            ans=-1
+            n=len(nums)
+            r= n-1
+            l=0
+            while(l<=r):
+                m=(l+r)//2
+                if nums[m]==target:
+                    ans=m
+                    if isStart:
+                        r=m-1
+                    else:
+                        l=m+1
+                elif nums[m]>target:
+                    r=m-1
+                else:
+                    l=m+1
+            return ans
+
+        a=calc(nums,target,True)
+        b=calc(nums,target,False)  
+        return [a,b]
         
