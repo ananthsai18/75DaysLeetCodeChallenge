32f9e0bfb1071edf82bbbc2a3711fc66c4e4089f
Ananthsai <ananthsaigudla1718@gmail.com>
2026-03-24 17:46:43 +0530

    Time: 0 ms (100%), Space: 19.4 MB (89.78%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,37 +1,28 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         n=len(nums)
-        l=0
-        r=n-1
-        mini=0
-        while(l<=r):
-            m=(l+r)//2
-            if nums[m]<=nums[n-1]:
-                mini=m
-                r=m-1
+        low=0
+        high=n-1
+        while(low<=high):
+            guess=(low+high)//2
+            if nums[guess]==target:
+                return guess
+            elif nums[guess]<nums[n-1]:
+                if nums[guess]>target:
+                    high=guess-1
+                else:
+                    if target >nums[n-1]:
+                        high=guess-1
+                    else:
+                        low=guess+1
             else:
-                l=m+1
-
-
-        l=0
-        r=mini
-        while(l<=r):
-            m=(l+r)//2
-            if nums[m]==target:
-                return m
-            elif nums[m]>target:
-                r=m-1
-            else:
-                l=m+1
-
-        l=mini
-        r=n-1
-        while(l<=r):
-            m=(l+r)//2
-            if nums[m]==target:
-                return m
-            elif nums[m]>target:
-                r=m-1
-            else:
-                l=m+1
-        return -1  
+                if nums[guess]<target:
+                    low=guess+1
+                else:
+                    if nums[0]>target:
+                        low=guess+1
+                    else:
+                        high=guess-1
+        return -1
+           
+        
