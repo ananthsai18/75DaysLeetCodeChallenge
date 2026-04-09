96dede8e39af0910391f63da18fc99d281e38b21
Ananthsai <ananthsaigudla1718@gmail.com>
2026-04-09 13:04:58 +0530

    Time: 0 ms (100%), Space: 19.3 MB (72.42%) - LeetHub

--- 3046-split-the-array/3046-split-the-array.py
+++ 3046-split-the-array/3046-split-the-array.py
@@ -1,12 +1,14 @@
 class Solution:
     def isPossibleToSplit(self, nums: List[int]) -> bool:
+        if len(nums) % 2 != 0:
+            return False
         dic={}
         for i in nums:
             if i not in dic:
                 dic[i]=1
             else:
                 dic[i]+=1
-        print(dic)
+      
         for i in dic:
             if dic[i]>2:
                 return False
