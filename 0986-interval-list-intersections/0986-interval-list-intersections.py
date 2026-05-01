c7cfdd04b3816c2a5f5fbef670f0053faa01b083
Ananthsai <ananthsaigudla1718@gmail.com>
2026-05-01 22:36:59 +0530

    Time: 0 ms (100%), Space: 19.9 MB (97.13%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.py
+++ 0986-interval-list-intersections/0986-interval-list-intersections.py
@@ -6,29 +6,30 @@ class Solution:
         j=0
         res=[]
 
-        while((i<n) and (j<m)):
+        while (i<n) and (j<m):
+
             s1=firstList[i][0]
             e1=firstList[i][1]
 
             s2=secondList[j][0]
             e2=secondList[j][1]
 
-            if s1 <= s2 :
-                if(e1>=s2):
-                    p=max(s1,s2)
-                    q=min(e1,e2)
-                    res.append([p,q])
-            
-            elif s1 > s2 :
-                if (e2>=s1):
-                    p=max(s1,s2)
-                    q=min(e1,e2)
-                    res.append([p,q])
-            
-            if e1<=e2:
-                i+=1
-            else:
-                j+=1
-        return res
+            if s1<=s2:
 
-        
+                if e1>=s2:
+                    p=max(s1,s2)
+                    q=min(e1,e2)
+                    res.append([p,q])
+
+            elif s1>s2:
+
+                if e2>=s1:
+                    p=max(s1,s2)
+                    q=min(e1,e2)
+                    res.append([p,q])
+
+            if e1 >=e2:
+                j+=1
+            else:
+                i+=1
+        return res
