681e3b3bbbd867fba83571285277f0b5082468ad
Ananthsai <ananthsaigudla1718@gmail.com>
2026-06-25 07:58:16 +0530

    Time: 15 ms (5.6%), Space: 48 MB (9.25%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -5,16 +5,8 @@ class Solution {
 
         for(int i=0;i<sentences.length;i++){
             String k=sentences[i];
-            int temp=0;
             String arr[]=k.split("[\\s]+");
-            // for(int p=0;p<arr.length;p++){
-            //     System.out.println(arr[p]);
-            // }
-
-            for(int j=0;j<arr.length;j++){
-                temp+=1;
-            }
-            ans=Math.max(ans,temp);
+            ans=Math.max(ans,arr.length);
         }
 
         return ans;
